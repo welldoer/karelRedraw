c8e7ec0c3165f626c94a7b7a66c47657ec15f7f9
welldoer <welldoer@263.net>
2021-03-03 16:05:55 +0800

    在 IDEA 中创建新的项目，添加本地、阿里镜像服务

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # karelRedraw
+
+## 初始环境准备清单
+~~1. 在 GitHub 中新建 repo~~
+
+~~2. 在本地 Clone repo~~
+
+~~3，在 IDEA 开发环境中基于以上项目目录创建新项目~~
+
+~~4. 在 build.gradle.kts 中引入本地、阿里镜像服务~~
+
+5. 在 Gradle 中整合 Sonar、jocco，并借助 GitAction 实现 CI
+
+
+## 特别参考：
+[karel](https://github.com/fredoverflow/karel.git)
---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.30"
+}
+
+group = "net.blogjava.welldoer"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenLocal()
+    maven {
+        url = uri("https://maven.aliyun.com/repository/public/")
+    }
+    maven {
+        url = uri("https://maven.aliyun.com/repository/central")
+    }
+    maven {
+        url = uri("https://maven.aliyun.com/repository/gradle-plugin")
+    }
+
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test-junit5"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
