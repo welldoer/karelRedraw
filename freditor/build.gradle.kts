8d8b1a8e4a79961cf9e2f10de5d9e467b7517525
welldoer <welldoer@263.net>
2021-03-04 16:22:43 +0800

    [Green]增加 freditor 模块，并添加 IntVector 类及测试

---
+++ freditor/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    `java-library`
+}
+
+repositories {
+    mavenLocal()
+    maven {
+        url = uri("https://maven.aliyun.com/repository/public/")
+    }
+    maven {
+        url = uri("https://maven.aliyun.com/repository/central")
+    }
+    maven {
+        url = uri("https://maven.aliyun.com/repository/gradle-plugin")
+    }
+
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
